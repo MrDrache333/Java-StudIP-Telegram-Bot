02678d3b549a9757388ac77fd88ba971f134bb08
Denis <denis.groeneveld@uni-oldenburg.de>
2022-12-16 01:16:31 +0100

    Bugfix for Drive Connector (#14)
    * This needs to be the plain name.
    
    * Bugfix for Drive Connect

--- src/main/java/de/oelrichsgarcia/studipTelegramBot/studipTelegramBot/studip/StudIPBot.java
+++ src/main/java/de/oelrichsgarcia/studipTelegramBot/studipTelegramBot/studip/StudIPBot.java
@@ -323,7 +323,7 @@ public class StudIPBot {
                 for (char ch : fullPath.toCharArray()) {
 
                     fullPath = fullPath.substring(1);
-                    if (ch == '\\') {
+                    if (ch == '\\' || ch == '/') {
                         break;
                     } else {
                         subfolderTmp.append(ch);
@@ -353,6 +353,7 @@ public class StudIPBot {
 
         } catch (Exception e) {
             Sout("Failed to upload " + path + "/" + file.getName());
+            Sout(String.valueOf(e));
         }
     }
 
